3986efb75f8e8486ff7cdb3d87420278a851e16f
solankirishav21 <btech10645.20@bitmesra.ac.in>
2021-09-30 19:22:12 +0530

    update app.js

--- js/app.js
+++ js/app.js
@@ -52,11 +52,11 @@ function gameEngine(){
     if(snakeArr[0].y === food.y && snakeArr[0].x ===food.x){
         foodSound.play();
         score += 1;
-        if(score>hiscoreval){
-            hiscoreval = score;
-            localStorage.setItem("hiscore", JSON.stringify(hiscoreval));
-            // score_.innerHTML = "HiScore: " + hiscoreval;
-        }
+        // if(score>hiscoreval){
+        //     hiscoreval = score;
+        //     localStorage.setItem("hiscore", JSON.stringify(hiscoreval));
+        //     // score_.innerHTML = "HiScore: " + hiscoreval;
+        // }
         scoreBox.innerHTML = "Score: " + score;
         snakeArr.unshift({x: snakeArr[0].x + inputDir.x, y: snakeArr[0].y + inputDir.y});
         let a = 2;
